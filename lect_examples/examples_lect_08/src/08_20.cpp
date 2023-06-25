843cd44b2b9a29c00c2ffb1dfdb9f52d5ecb60fc
doctor <nikitalystsev@yandex.ru>
2023-06-25 15:51:17 +0300

    усиланно ботаю

---
+++ lect_examples/examples_lect_08/src/08_20.cpp
@@ -0,0 +1,21 @@
+// Пример 08.19. Шаблон функции с переменным числом параметров.
+#include <iostream>
+
+using namespace std;
+
+template <typename Type>
+Type sum(Type value)
+{
+    return value;
+}
+
+template <typename Type, typename... Args>
+Type sum(Type value, Args... params)
+{
+    return value + sum(params...);
+}
+
+int main()
+{
+    cout << sum(1, 2, 3, 4, 5) << endl;
+}
